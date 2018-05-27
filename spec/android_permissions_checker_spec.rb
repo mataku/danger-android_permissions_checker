131e7c1e37ea54188fed5b16e271c28e3dbd9028
Takuma Homma <nagomimatcha@gmail.com>
2018-05-27 23:09:37 +0900

    Add spec

--- spec/android_permissions_checker_spec.rb
+++ spec/android_permissions_checker_spec.rb
@@ -2,45 +2,87 @@ require File.expand_path("../spec_helper", __FILE__)
 
 module Danger
   describe Danger::DangerAndroidPermissionsChecker do
+    let(:dangerfile) { testing_dangerfile }
+    let(:plugin) { dangerfile.android_permissions_checker }
+    
     it "should be a plugin" do
       expect(Danger::DangerAndroidPermissionsChecker.new(nil)).to be_a Danger::Plugin
     end
 
-    #
-    # You should test your custom attributes and methods here
-    #
-    describe "with Dangerfile" do
+    describe "#check" do
+
+      let(:tmp_file) { '/tmp/permissions.txt' }
+      let(:current_permission_file) { 'permissions.txt' }
+      let(:apk) { '/tmp/app.apk' }
+      let(:aapt_command) { "aapt d permissions #{apk}" }
+      let(:current_permissions) do
+        "package: com.mataku.scrobscrob.dev\nuses-permission: name='android.permission.INTERNET'\n"
+      end
+
+      let(:generated_permissions_list) do
+        [
+          "package: com.mataku.scrobscrob.dev\n",
+          "uses-permission: name='android.permission.INTERNET'\n"
+        ]
+      end
+
       before do
-        @dangerfile = testing_dangerfile
-        @my_plugin = @dangerfile.android_permissions_checker
-
-        # mock the PR data
-        # you can then use this, eg. github.pr_author, later in the spec
-        json = File.read(File.dirname(__FILE__) + '/support/fixtures/github_pr.json') # example json: `curl https://api.github.com/repos/danger/danger-plugin-template/pulls/18 > github_pr.json`
-        allow(@my_plugin.github).to receive(:pr_json).and_return(json)
+        allow(current_permission_file).to receive(:nil?).and_return(false) 
+        allow(apk).to receive(:nil?).and_return(false)
+        allow(File).to receive(:exist?).with(current_permission_file).and_return(true)
+        allow(File).to receive(:exist?).with(apk).and_return(true)
+        allow_any_instance_of(Kernel).to receive(:system).with('which aapt > /dev/null 2>&1').and_return(true)
+        allow_any_instance_of(Kernel).to receive(:`).with(aapt_command).and_return(current_permissions)
+        allow(File).to receive_message_chain(:open, :readlines).with(current_permission_file).with(no_args).and_return(generated_permissions_list)
       end
 
-      # Some examples for writing tests
-      # You should replace these with your own.
-
-      it "Warns on a monday" do
-        monday_date = Date.parse("2016-07-11")
-        allow(Date).to receive(:today).and_return monday_date
-
-        @my_plugin.warn_on_mondays
-
-        expect(@dangerfile.status_report[:warnings]).to eq(["Trying to merge code on a Monday"])
+      it do
+        plugin.check(apk: apk, permission_list_file: current_permission_file)
+        expect(dangerfile.status_report[:warnings].length).to eq(0)
       end
 
-      it "Does nothing on a tuesday" do
-        monday_date = Date.parse("2016-07-12")
-        allow(Date).to receive(:today).and_return monday_date
+      context 'Permission deleted' do
+        let(:current_permissions) do
+          "package: com.mataku.scrobscrob.dev\nuses-permission: name='android.permission.INTERNET'\nuses-permission: name='com.mataku.INTERNET'\n"
+        end
 
-        @my_plugin.warn_on_mondays
+        it do
+          plugin.check(apk: apk, permission_list_file: current_permission_file)
+          expect(dangerfile.status_report[:warnings].length).to eq(1)
+          expect(dangerfile.status_report[:warnings][0]).to eq('APK permissions changed, see below.')
+          expect(dangerfile.status_report[:markdowns][0].message).to include('Deleted')
+          expect(dangerfile.status_report[:markdowns][0].message).not_to include('Added')
 
-        expect(@dangerfile.status_report[:warnings]).to eq([])
+        end
       end
 
+      context 'Permission added' do
+        let(:current_permissions) do
+          "package: com.mataku.scrobscrob.dev\n"
+        end
+
+        it do
+          plugin.check(apk: apk, permission_list_file: current_permission_file)
+          expect(dangerfile.status_report[:warnings].length).to eq(1)
+          expect(dangerfile.status_report[:warnings][0]).to eq('APK permissions changed, see below.')
+          expect(dangerfile.status_report[:markdowns][0].message).not_to include('Deleted')
+          expect(dangerfile.status_report[:markdowns][0].message).to include('Added')
+        end
+      end
+
+      context 'Permission added' do
+        let(:current_permissions) do
+          "package: com.mataku.scrobscrob.dev\nuses-permission: name='com.mataku.INTERNET'"
+        end
+
+        it do
+          plugin.check(apk: apk, permission_list_file: current_permission_file)
+          expect(dangerfile.status_report[:warnings].length).to eq(1)
+          expect(dangerfile.status_report[:warnings][0]).to eq('APK permissions changed, see below.')
+          expect(dangerfile.status_report[:markdowns][0].message).to include('Deleted')
+          expect(dangerfile.status_report[:markdowns][0].message).to include('Added')
+        end
+      end
     end
   end
 end
